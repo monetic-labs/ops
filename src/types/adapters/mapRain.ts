7c0112a354e620c457de4ea09a34137228861fd9
Thomas Humphreys <thomasehumphreys@gmail.com>
2024-10-23 13:15:34 +0800

    chore: bump pkg + fix build errors

--- src/types/adapters/mapRain.ts
+++ src/types/adapters/mapRain.ts
@@ -1,120 +1,120 @@
 // mappers/rainMapper.ts
 
 import {
-    CompanyAccountSchema,
-    CompanyDetailsSchema,
-    CompanyAccountUsersSchema,
-    CompanyUserDetailsSchema,
-  } from '@/types/validations/onboard';
-  import {
-    RainPersonDto,
-    RainMerchantCreateDto,
-    RainInitialUserDto,
-    RainEntityDto,
-    RainAddressDto,
-  } from '@/types/dtos/rainDTO';
+  CompanyAccountSchema,
+  CompanyDetailsSchema,
+  CompanyAccountUsersSchema,
+  CompanyUserDetailsSchema,
+} from "@/types/validations/onboard";
+import {
+  RainPersonDto,
+  RainMerchantCreateDto,
+  RainInitialUserDto,
+  RainEntityDto,
+  RainAddressDto,
+} from "@/types/dtos/rainDTO";
 
-  function formatSSN(ssn: string): string {
-    return ssn.replace(/-/g, '');
+function formatSSN(ssn: string): string {
+  return ssn.replace(/-/g, "");
+}
+
+export function mapToRainMerchantCreateDto(
+  accountData: CompanyAccountSchema,
+  detailsData: CompanyDetailsSchema,
+  usersData: CompanyAccountUsersSchema,
+  userDetailsData: CompanyUserDetailsSchema,
+  additionalData: {
+    isTermsOfServiceAccepted: boolean;
+    expectedSpend: string;
+    id: string;
   }
-  
-  export function mapToRainMerchantCreateDto(
-    accountData: CompanyAccountSchema,
-    detailsData: CompanyDetailsSchema,
-    usersData: CompanyAccountUsersSchema,
-    userDetailsData: CompanyUserDetailsSchema,
-    additionalData: {
-      isTermsOfServiceAccepted: boolean;
-      expectedSpend: string;
-      id: string;
-    }
-  ): RainMerchantCreateDto {
-    const { company } = accountData;
-    const { walletAddress, companyEIN, companyType, companyDescription } = detailsData;
-    const { representatives } = usersData;
-    const { userDetails } = userDetailsData;
-  
-    // Map company registered address to RainAddressDto
-    const companyAddress: RainAddressDto = {
-      line1: company.registeredAddress.street1,
-      line2: company.registeredAddress.street2,
-      city: company.registeredAddress.city,
-      region: company.registeredAddress.state || '',
-      postalCode: company.registeredAddress.postcode,
-      countryCode: company.registeredAddress.country,
-      country: company.registeredAddress.country,
-    };
-  
-    // Map entity data to RainEntityDto
-    const entity: RainEntityDto = {
-      name: company.name,
-      type: companyType,
-      description: companyDescription,
-      taxId: companyEIN,
-      website: company.website,
-      expectedSpend: additionalData.expectedSpend,
-    };
+): RainMerchantCreateDto {
+  const { company } = accountData;
+  const { walletAddress, companyEIN, companyType, companyDescription } = detailsData;
+  const { representatives } = usersData;
+  const { userDetails } = userDetailsData;
 
-    // We use role to map which user object but the service doesn't want it, we use this to manage that
-    type RainPersonWithRole = RainPersonDto & {
-        role: "owner" | "representative" | "beneficial-owner";
-        //role: string;
-    }
-  
-    // Map representatives to RainPersonDto[]
-    const rainRepresentatives: RainPersonWithRole[] = representatives.map((rep, index) => {
-      const userDetail = userDetails[index]; // Assuming the order matches
-      return {
-        id: additionalData.id,
-        firstName: rep.firstName,
-        lastName: rep.lastName,
-        birthDate: userDetail.birthday,
-        nationalId: formatSSN(userDetail.ssn),
-        countryOfIssue: userDetail.countryOfIssue,
-        email: rep.email,
-        address: {
-          line1: userDetail.registeredAddress.street1,
-          line2: userDetail.registeredAddress.street2,
-          city: userDetail.registeredAddress.city,
-          region: userDetail.registeredAddress.state || '',
-          postalCode: userDetail.registeredAddress.postcode,
-          countryCode: userDetail.registeredAddress.country,
-          country: userDetail.registeredAddress.country,
-        },
-        role: rep.role as "owner" | "representative" | "beneficial-owner",
-      };
-    });
-  
-    // Map the initial user (first representative)
-    const initialUser: RainInitialUserDto = {
-      ...rainRepresentatives[0], // Use the first representative's data
-      isTermsOfServiceAccepted: additionalData.isTermsOfServiceAccepted,
-      role: representatives[0].role,
-      walletAddress: walletAddress,
-    };
+  // Map company registered address to RainAddressDto
+  const companyAddress: RainAddressDto = {
+    street1: company.registeredAddress.street1,
+    street2: company.registeredAddress.street2,
+    city: company.registeredAddress.city,
+    region: company.registeredAddress.state || "",
+    postalCode: company.registeredAddress.postcode,
+    countryCode: company.registeredAddress.country,
+    country: company.registeredAddress.country,
+  };
 
-    const ultimateBeneficialOwners: RainPersonDto[] = rainRepresentatives
-    .filter((rep) => rep.role === 'beneficial-owner')
+  // Map entity data to RainEntityDto
+  const entity: RainEntityDto = {
+    name: company.name,
+    type: companyType,
+    description: companyDescription,
+    taxId: companyEIN,
+    website: company.website,
+    expectedSpend: additionalData.expectedSpend,
+  };
+
+  // We use role to map which user object but the service doesn't want it, we use this to manage that
+  type RainPersonWithRole = RainPersonDto & {
+    role: "owner" | "representative" | "beneficial-owner";
+    //role: string;
+  };
+
+  // Map representatives to RainPersonDto[]
+  const rainRepresentatives: RainPersonWithRole[] = representatives.map((rep, index) => {
+    const userDetail = userDetails[index]; // Assuming the order matches
+    return {
+      id: additionalData.id,
+      firstName: rep.firstName,
+      lastName: rep.lastName,
+      birthDate: userDetail.birthday,
+      nationalId: formatSSN(userDetail.ssn),
+      countryOfIssue: userDetail.countryOfIssue,
+      email: rep.email,
+      address: {
+        street1: userDetail.registeredAddress.street1,
+        street2: userDetail.registeredAddress.street2,
+        city: userDetail.registeredAddress.city,
+        region: userDetail.registeredAddress.state || "",
+        postalCode: userDetail.registeredAddress.postcode,
+        countryCode: userDetail.registeredAddress.country,
+        country: userDetail.registeredAddress.country,
+      },
+      role: rep.role as "owner" | "representative" | "beneficial-owner",
+    };
+  });
+
+  // Map the initial user (first representative)
+  const initialUser: RainInitialUserDto = {
+    ...rainRepresentatives[0], // Use the first representative's data
+    isTermsOfServiceAccepted: additionalData.isTermsOfServiceAccepted,
+    role: representatives[0].role,
+    walletAddress: walletAddress,
+  };
+
+  const ultimateBeneficialOwners: RainPersonDto[] = rainRepresentatives
+    .filter((rep) => rep.role === "beneficial-owner")
     .map(({ role, ...rest }) => rest); // Exclude 'role' from the final DTOs
-  
-    let representativesWithoutRole: RainPersonDto[] = rainRepresentatives
-    .filter((rep) => rep.role === 'representative' || rep.role === 'owner')
+
+  let representativesWithoutRole: RainPersonDto[] = rainRepresentatives
+    .filter((rep) => rep.role === "representative" || rep.role === "owner")
     .map(({ role, ...rest }) => rest);
 
-    // If there are no representatives, use the initial user as a representative
-    if (representatives.length === 0) {
-      const { isTermsOfServiceAccepted, role, walletAddress, ...initialUserWithoutExtra } = initialUser;
-      representativesWithoutRole = [initialUserWithoutExtra];
-    }
-    // Construct the final RainMerchantCreateDto object
-    const rainMerchantCreateDto: RainMerchantCreateDto = {
-      initialUser,
-      address: companyAddress,
-      entity,
-      name: company.name,
-      representatives: representativesWithoutRole,
-      ultimateBeneficialOwners,
-    };
-  
-    return rainMerchantCreateDto;
+  // If there are no representatives, use the initial user as a representative
+  if (representatives.length === 0) {
+    const { isTermsOfServiceAccepted, role, walletAddress, ...initialUserWithoutExtra } = initialUser;
+    representativesWithoutRole = [initialUserWithoutExtra];
   }
+  // Construct the final RainMerchantCreateDto object
+  const rainMerchantCreateDto: RainMerchantCreateDto = {
+    initialUser,
+    address: companyAddress,
+    entity,
+    name: company.name,
+    representatives: representativesWithoutRole,
+    ultimateBeneficialOwners,
+  };
+
+  return rainMerchantCreateDto;
+}
