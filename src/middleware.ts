13f833c15d5cd15f16d613da3c3daf85d4e642c3
Thomas Humphreys <thomasehumphreys@gmail.com>
2024-12-30 16:57:27 +0700

    fix: middleware check compliance bun lockb

--- src/middleware.ts
+++ src/middleware.ts
@@ -64,7 +64,6 @@ export const config = {
      * Match all request paths except for the ones starting with:
      * - api (API routes)
      * - _next/static (static files)
-     * - _next/static (static files)
      * - _next/image (image optimization files)
      * - favicon.ico (favicon file)
      * - bg-celestial.png (background image)
